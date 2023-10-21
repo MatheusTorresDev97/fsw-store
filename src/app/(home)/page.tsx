97ab190ed14483ad5354e083eae63c3606ae5b94
Matheus <matheustorresdev@gmail.com>
2023-10-21 18:11:43 -0300

    feat: add second banner to home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,32 +1,42 @@
-import Image from "next/image"
-import Categories from "./components/categories"
-import { prismaClient } from "@/lib/prisma"
-import ProductList from "./components/product-list"
+import Image from "next/image";
+import Categories from "./components/categories";
+import { prismaClient } from "@/lib/prisma";
+import ProductList from "./components/product-list";
 
 export default async function Home() {
-    const deals = await prismaClient.product.findMany({
-        where: {
-            discountPercentage: {
-                gt: 0,
-            }
-        }
-    })
-    return (
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+  return (
     <div>
-        <Image
-        src='/banner-home-01.png'
+      <Image
+        src="/banner-home-01.png"
         height={0}
         width={0}
         className="h-auto w-full px-5"
         sizes="100vw"
         alt=""
-        />
-        <div className="mt-8 px-5">
+      />
+      <div className="mt-8 px-5">
         <Categories />
-        </div>
-        <div className="mt-8">
-            <ProductList products={deals}/>
-        </div>
+      </div>
+      <div className="mt-8">
+        <p className="mb-2 pl-5 font-bold uppercase">Ofertas</p>
+        <ProductList products={deals} />
+      </div>
+
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        alt=""
+      />
     </div>
-    ) 
+  );
 }
