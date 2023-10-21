a16e4b3a8b92cc7d240102ae1abde38eaeee83ac
Matheus <matheustorresdev@gmail.com>
2023-10-21 19:13:26 -0300

    feat: include footer

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -37,7 +37,7 @@ export default async function Home() {
         alt="Até 55% de desconto esse mês!"
       />
 
-      <div className="px-5">
+      <div className="px-5"> 
         <Categories />
       </div>
 
